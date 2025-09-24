1326bf06f4ae287af6077a50a66b8adb453922bc
Josh Johnson <josh.johnson0388@gmail.com>
2025-09-24 17:35:01 -0400

    update, not finished just saving

--- main.c
+++ main.c
@@ -1,79 +1,97 @@
-  #include <stdio.h>
-  #include <unistd.h>
-  #include <sys/wait.h>
-  #include <string.h>
-  #include <stdlib.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
-  /*
+#define READ_END 0
+#define WRITE_END 1
+#define MAXMSGLENGTH 500
 
-  CIS 452 01
-  Kenan Velagic
-  Joshua Johnson
-  Project One - One Bad Apple
+/*
 
-  */
+CIS 452 01
+Kenan Velagic
+Joshua Johnson
+Project One - One Bad Apple
 
-  int main() {
-      char input[500];
-      pid_t pid;
-      int k;
+*/
 
-      /*
-      .
-      .
-      .
-        Prompt user to enter the number of nodes for the 
-        circular ring. Store the value in var 'k', which will 
-        later be used to create that many processes and pipes.
-      .
-      .
-      .
-      */
+int main() {
+    char input[500];
+    pid_t pid;
+    int numProcesses;
 
-      printf("Enter the number of nodes: ");
-      scanf("%d", &k);
-      getchar(); // Clear buffer
+    /*
+    .
+    .
+    .
+      Prompt user to enter the number of nodes for the
+      circular ring. Store the value in var 'k', which will
+      later be used to create that many processes and pipes.
+    .
+    .
+    .
+    */
 
-      /*
-      .
-      .
-      .
-        After getting the number of nodes k, we create k pipes.
-        Each pipe will connect one node to the next.
-         - pipes[i][0] is the read end of pipe i
-         - pipes[i][1] is the write end of pipe i
-        These pipes allow each child process (node) to receive a message
-        from the previous, and send it to the next node in the ring.
-      .
-      .
-      .
-      */
+    printf("Enter the number of nodes: ");
+    scanf("%d", &numProcesses);
+    getchar();  // Clear buffer
 
-      int pipes[k][2];
+    /*
+    .
+    .
+    .
+      After getting the number of nodes k, we create k pipes.
+      Each pipe will connect one node to the next.
+       - pipes[i][0] is the read end of pipe i
+       - pipes[i][1] is the write end of pipe i
+      These pipes allow each child process (node) to receive a message
+      from the previous, and send it to the next node in the ring.
+    .
+    .
+    .
+    */
 
-      for (int i = 0; i < k; i++) {
+    puts("Enter message you would like to send");
+    fgets(input, sizeof(input), stdin);
+
+    int pipes[numProcesses][2];
+
+    for (int i = 0; i < numProcesses; i++) {
         if (pipe(pipes[i]) < 0) {
-          perror("Failed pipe creation.");
-          exit(1);
+            perror("Failed pipe creation.");
+            exit(1);
         }
-      }
+    }
 
-      for (int i = 0; i < k; i++) {
+    int nodeId = 0;
+
+    for (int i = 1; i < numProcesses; i++) {
         pid = fork();
-
         if (pid < 0) {
-          perror("Unsuccessful fork system call.");
-          exit(1);
+            perror("Unsuccessful fork system call.");
+            exit(1);
         }
-
         if (pid == 0) {
-          // Child Process
-
-        } else {
-          // Parent Process
-          wait(NULL);
+            nodeID = i;
+            break;
         }
-      }
-      
-      return 0;
-  }
+    }
+    int next = (nodeID + 1) % numProcesses
+    int readFd = pipes[nodeID][READ_END];  // read from predecessor
+    int writeFd = pipes[next][WRITE_END];    // write to next
+
+    // close all other pipes
+    for (int j = 0; j < numProcesses; ++j) {
+        if (j != nodeID) close(pipes[j][0]);
+        if (j != (nodeID + 1) % numProcesses) close(pipes[j][1]);
+    }
+    
+
+    // ring loop
+    while (1) {
+    }
+    return 0;
+}
